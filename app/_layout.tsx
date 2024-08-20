d9ac262448b7762a70c96c015d24b13d478b3c2d
Kimoto-Norihiro <n000r111code@gmail.com>
2024-08-20 17:24:17 +0900

    fix

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { Stack } from 'expo-router/stack';
-import ExpoCameraComponent from '../components/ExpoCameraComponent';
-import Camera from './camera';
 
 export default function Layout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="index" />
       <Stack.Screen name="camera" options={{ headerShown: false }} />
     </Stack>
   );
